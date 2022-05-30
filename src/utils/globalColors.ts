ae2253c253a87d52b723fae86b05d97d0a25b2fe
Timofey Boyko <fognir742@gmail.com>
2022-05-30 18:57:32 +0300

    Utils: add global colors

---
+++ src/utils/globalColors.ts
@@ -0,0 +1,19 @@
+const globalColors = {
+  text: "#333333",
+  disabledText: "#AEAEAE",
+  errorText: "#F21C0E",
+
+  border: "#D0D5DA",
+  borderHover: "#2DA7DB",
+  borderActive: "#3DB8EC",
+  borderDisabled: "#ECEEF1",
+  borderError: "#F21C0E",
+
+  background: "#FFFFFF",
+  backgroundHover: "#FFFFFF",
+  backgroundActive: "#ECEEF1",
+  backgroundDisabled: "#F8F9F9",
+  backgroundError: "#FFFFFF",
+};
+
+export default globalColors;
---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+import { default as GlobalColors } from "./globalColors";
+
+const utils = { globalColors: GlobalColors };
+
+export default utils;
