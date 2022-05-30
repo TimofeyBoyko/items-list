907101c9e49ff2cc2cca60e60506d7f380506c40
Timofey Boyko <fognir742@gmail.com>
2022-05-30 19:33:37 +0300

    Components: update exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import Text from "./Text";
+import Button from "./Button";
+import IconButton from "./IconButton";
 
-const components = { Text: Text };
+const components = { Text: Text, Button: Button, IconButton: IconButton };
 
 export default components;
