601403f55ecf429155eebc0735cafdcb732fb4ef
Timofey Boyko <fognir742@gmail.com>
2022-05-31 20:24:42 +0300

    Api: add fetchCardById function

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,29 @@
 import axios from "axios";
 
+interface IFetchCard {
+  id: string;
+  url: string;
+  author: string;
+  download_url: string;
+  height: number;
+  width: number;
+}
+
 export async function fetchCardList() {
-  const data = await (await axios.get("https://picsum.photos/v2/list")).data;
+  const res = await axios.get("https://picsum.photos/v2/list");
+
+  const data = res.data.map((item: IFetchCard) => ({
+    ...item,
+    isFavorite: false,
+  }));
+
+  return data;
+}
+
+export async function fetchCardById(id: string) {
+  const res = await axios.get(`https://picsum.photos/id/${id}/info`);
+
+  const data = res.data;
 
   return data;
 }
