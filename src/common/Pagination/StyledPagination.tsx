73e719d065ed69b9b4004d12e94b8b4e7912efca
Timofey Boyko <fognir742@gmail.com>
2022-05-31 21:25:50 +0300

    Common: add pagination component

---
+++ src/common/Pagination/StyledPagination.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledPaginationContainer = styled.div`
+  width: 100%;
+  height: 50px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0 20px;
+
+  box-sizing: border-box;
+`;
+
+export default StyledPaginationContainer;
